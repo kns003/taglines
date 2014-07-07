607cd69dc2b40eeafe5517f688804ee7576ba9d3
Shashank <shashank.hegde@tarams.com>
2014-07-07 14:25:51 +0530

    register admin

--- kannada/admin.py
+++ kannada/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
+from . import Movie
 # Register your models here.
 
-admin.site.register(kannada)
+admin.site.register(Movie)
--- kannada/models.py
+++ kannada/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    name = models.CharField(max_length = 100)
+    tagline = models.CharField(max_length = 250)
+
+    def __unicode__(self):
+        return self.name
+    
